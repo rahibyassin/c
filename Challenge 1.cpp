5b586a68c4b06774fd8cdf8b1d5fe14f70cc4259
yassine rahib <rahibyassine@gmail.com>
2021-11-10 16:17:14 +0100

    Challenge 1.cpp

---
+++ Challenge 1.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv)
+{char nom[20];
+    char prenom[20];
+    char age[20];
+    char sexe[20];
+    char tele[20];
+
+	printf("Votre Nom\n");
+    scanf("%s", nom);
+
+    printf("Votre Prenom\n");
+    scanf("%s", prenom);
+
+    printf("Votre Age\n");
+    scanf("%s", age);
+
+    printf("Votre Sexe\n");
+    scanf("%s", sexe);
+
+    printf("Votre Tele\n");
+    scanf("%s", tele);
+
+
+    printf("Nom Complet : %s %s, Age : %s, Sexe : %s, Tele : %s\n", nom, prenom, age, sexe, tele);
+	system("pause");
+}
